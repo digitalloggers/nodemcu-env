49290d747d97a92888f25960742b1bccd580bb32
Sergey Zakharchenko <szakharchenko@open-sys.org>
2015-09-28 06:47:34 +0000

    Integrating luac.cross into build.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 HERE=`dirname $0`
 HERE=`realpath $HERE`
+if [ $# -ge 1 ]; then
+    LUA="$1";shift
+else
+    LUA=lua
+fi
 {
     cd $HERE/esp-open-sdk
     make
@@ -8,4 +13,5 @@ HERE=`realpath $HERE`
 {
     cd $HERE/nodemcu-firmware
     PATH=$HERE/esp-open-sdk/xtensa-lx106-elf/bin:$PATH make
+    PATH=$HERE/esp-open-sdk/xtensa-lx106-elf/bin:$PATH $LUA tools/cross-lua.lua
 }
